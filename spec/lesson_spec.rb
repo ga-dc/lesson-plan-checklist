238f34f38f7db31ecf6566c61e47a91c02ae6529
Jesse Shawl <jesse@jshawl.com>
2016-01-28 14:35:59 -0500

    add framing and los

--- spec/lesson_spec.rb
+++ spec/lesson_spec.rb
@@ -1,5 +1,12 @@
 describe "Lesson Plan" do
+  let(:readme) {File.read("readme.md")}
   it "has a readme.md" do
-    expect(File).to exist("../readme.md")
+    expect(File).to exist("./readme.md")
+  end
+  it "has learning objectives" do
+    expect(readme).to match "## Learning Objectives"
+  end
+  it "has Framing" do
+    expect(readme).to match "## Framing"
   end
 end
